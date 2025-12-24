5f1dedc3d74597515994c14ebd83f5fef9aa98d4
ahmdzaidan <ahmadzanshory@gmail.com>
2025-12-24 21:39:40 +0700

    commit ke 3

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,41 +1,29 @@
-import argparse
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import mlflow
 import mlflow.sklearn
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
+# Enable autologging untuk sklearn
+mlflow.sklearn.autolog()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--n_estimators", type=int, default=100)
-parser.add_argument("--max_depth", type=int, default=10)
-args = parser.parse_args()
-
+# Load dataset
 data = pd.read_csv("foodspoiled_preprocessing.csv")
+X = data.drop("Status", axis=1)
+y = data["Status"]
 
-target = "Status"
-X = data.drop(columns=[target])
-y = data[target]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
-
-mlflow.set_experiment("CI-Training")
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
 
 with mlflow.start_run():
-    model = RandomForestClassifier(
-        n_estimators=args.n_estimators,
-        max_depth=args.max_depth,
-        random_state=42
-    )
-
+    # Train model
+    model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
     model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
 
-    f1 = f1_score(y_test, y_pred)
-
-    mlflow.log_param("n_estimators", args.n_estimators)
-    mlflow.log_param("max_depth", args.max_depth)
-    mlflow.log_metric("f1_score", f1)
-
-    mlflow.sklearn.log_model(model, name="model")
+    # Predict & evaluate
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+    print(f"Accuracy: {acc}")
