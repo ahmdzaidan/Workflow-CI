c92be9f1bfe7f27cc6a86b39fa10ffc83a8012e4
ahmdzaidan <ahmadzanshory@gmail.com>
2025-12-24 22:05:14 +0700

    7 commit

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 import mlflow
 import mlflow.sklearn
+import joblib
 
 # Enable autologging
 mlflow.sklearn.autolog()
@@ -24,3 +25,6 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Fit model
 clf = RandomForestClassifier(n_estimators=100, random_state=42)
 clf.fit(X_train, y_train)
+
+joblib.dump(clf, "model.pkl")
+mlflow.log_artifact("model.pkl", artifact_path="model_file")
